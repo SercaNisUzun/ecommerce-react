f6a522f90dfdc3477b01ce51c56faaed23bdaf3b
SercaNisUzun <124430337+SercaNisUzun@users.noreply.github.com>
2024-12-24 22:41:22 +0300

    dist v2

--- src/config/RouterConfig.jsx
+++ src/config/RouterConfig.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { HashRouter as Router, Route, Routes } from 'react-router-dom'
+import { HashRouter as Route, Routes } from 'react-router-dom'
 import Home from '../Pages/Home'
 import Catalogue from '../Pages/Catalogue'
 import ProductDetails from '../components/ProductDetails'
